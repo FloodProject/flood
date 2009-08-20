98c0db5742d1e4991a91a70beef2039e7f05de30
João Matos <triton@vapor3d.org>
2009-08-20 00:36:08 +0000

    Add missing files (damn SVN).

---
+++ inc/vapor/input/Type.h
@@ -0,0 +1,36 @@
+/************************************************************************
+*
+* vaporEngine (2008-2009)
+*
+*	<http://www.portugal-a-programar.org>
+*
+************************************************************************/
+
+#pragma once
+
+#include "vapor/Platform.h"
+
+#include "vapor/input/Event.h"
+
+namespace vapor {
+	namespace input {
+
+//-----------------------------------//
+
+/**
+ * Different types of input devices.
+ */
+
+namespace DeviceType
+{
+	enum Enum
+	{
+		Keyboard,
+		Mouse,
+		Joystick
+	};
+}
+
+//-----------------------------------//
+
+} } // end namespaces
---
+++ src/Engine/Event.cpp
@@ -0,0 +1,24 @@
+/************************************************************************
+*
+* vaporEngine (2008-2009)
+*
+*	<http://www.portugal-a-programar.org>
+*
+************************************************************************/
+
+#include "vapor/input/Event.h"
+
+namespace vapor {
+	namespace input {
+
+//-----------------------------------//
+
+Event::Event( DeviceType::Enum type )
+	: deviceType( type )
+{
+
+}
+
+//-----------------------------------//
+
+} } // end namespaces
