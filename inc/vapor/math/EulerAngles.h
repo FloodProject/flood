3cc28846d76f958f36d1d3f31b1d4cf2340c58b6
João Matos <triton@vapor3d.org>
2009-10-05 13:37:26 +0000

    Forgot adding angles files.

---
+++ inc/vapor/math/EulerAngles.h
@@ -0,0 +1,55 @@
+/************************************************************************
+*
+* vaporEngine (2008-2009)
+*
+*	<http://www.portugal-a-programar.org>
+*
+************************************************************************/
+
+#pragma once
+
+#include "vapor/Platform.h"
+#include "vapor/math/Matrix.h"
+#include "vapor/math/Vector3.h"
+
+namespace vapor {
+	namespace math {
+
+//-----------------------------------//
+
+/**
+ * Representation of an orientation as a set of Euler angles that can be
+ * transformed into and back in a rotation matrix and quaternions.
+ */
+
+class VAPOR_API EulerAngles
+{
+public:
+
+	EulerAngles();
+
+	EulerAngles( float x, float y, float z );
+
+	// Adds the given angles to the existing angles.
+	void rotate( float x, float y, float z );
+
+	// Reduces the angles to the cannonical range (0-360º).
+	void canonize();
+
+	// Resets the rotations so they don't have any effect.
+	void identity();
+
+	// Gets a orientation matrix represeting the rotation angles.
+	math::Matrix4 getOrientationMatrix() const;
+
+	// many more methods implemented here ...
+	math::Matrix4 rotateX( float ang ) const;
+	math::Matrix4 rotateY( float ang ) const;
+	math::Matrix4 rotateZ( float ang ) const;
+
+	float xang, yang, zang;
+};
+
+//-----------------------------------//
+
+} } // end namespaces
---
+++ src/Engine/EulerAngles.cpp
@@ -0,0 +1,115 @@
+/************************************************************************
+*
+* vaporEngine (2008-2009)
+*
+*	<http://www.portugal-a-programar.org>
+*
+************************************************************************/
+
+#include "vapor/PCH.h"
+
+#include "vapor/math/EulerAngles.h"
+
+namespace vapor {
+	namespace math {
+
+//-----------------------------------//
+
+EulerAngles::EulerAngles()
+	: xang( 0.0f ), yang( 0.0f ), zang( 0.0f )
+{
+
+}
+
+//-----------------------------------//
+
+EulerAngles::EulerAngles( float x, float y, float z )
+	: xang( x ), yang( y ), zang( z )
+{
+
+}
+
+//-----------------------------------//
+
+void EulerAngles::rotate( float x, float y, float z )
+{
+	xang += x;
+	yang += y;
+	zang += z;
+}
+
+//-----------------------------------//
+
+void EulerAngles::canonize()
+{
+
+}
+
+//-----------------------------------//
+
+void EulerAngles::identity()
+{
+	xang = yang = zang = 0;
+}
+
+//-----------------------------------//
+
+math::Matrix4 EulerAngles::rotateX( float ang ) const
+{
+	Matrix4 newRotation;
+
+	newRotation.m11 = 1;
+
+	newRotation.m22 = math::cosf( ang );
+	newRotation.m23 = math::sinf( ang );
+
+	newRotation.m32 = -math::sinf( ang );
+	newRotation.m33 = math::cosf( ang );
+
+	return newRotation;
+}
+
+//-----------------------------------//
+
+math::Matrix4 EulerAngles::rotateY( float ang ) const
+{
+	Matrix4 newRotation;
+
+	newRotation.m11 = math::cosf( ang );
+	newRotation.m13 = -math::sinf( ang );
+
+	newRotation.m22 = 1;
+
+	newRotation.m31 = math::sinf( ang );
+	newRotation.m33 = math::cosf( ang );
+
+	return newRotation;
+}
+
+//-----------------------------------//
+
+math::Matrix4 EulerAngles::rotateZ( float ang ) const
+{
+	Matrix4 newRotation;
+
+	newRotation.m11 = math::cosf( ang );
+	newRotation.m12 = math::sinf( ang );
+
+	newRotation.m21 = -math::sinf( ang );
+	newRotation.m22 = math::cosf( ang );
+
+	newRotation.m33 = 1;
+
+	return newRotation;
+}
+
+//-----------------------------------//
+
+math::Matrix4 EulerAngles::getOrientationMatrix() const
+{
+	return rotateY( yang ) * rotateX( xang ) * rotateZ( zang );
+}
+
+//-----------------------------------//
+
+} } // end namespaces
