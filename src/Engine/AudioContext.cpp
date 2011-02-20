969419889ed09b41b8de46065ad6b8a89292406a
João Matos <triton@vapor3d.org>
2011-02-20 18:57:09 +0000

    Engine:  - Add corrupted files again.

---
+++ src/Engine/AudioContext.cpp
@@ -0,0 +1,158 @@
+/************************************************************************
+*
+* vapor3D Engine © (2008-2010)
+*
+*	<http://www.vapor3d.org>
+*
+************************************************************************/
+
+#include "vapor/PCH.h"
+
+#ifdef VAPOR_AUDIO_OPENAL
+
+#include "audio/Context.h"
+#include "audio/Device.h"
+
+#include <al.h>
+#include <alc.h>
+
+namespace vapor {
+
+//-----------------------------------//
+
+AudioContext::AudioContext(AudioDevice* device)
+	: device(device)
+	, context(nullptr)
+	, error(ALC_NO_ERROR)
+{
+	if(!device)
+	{
+		Log::warn("Invalid audio device.");
+		return;
+	}
+
+	context = createContext();
+
+	// Sets a default listener.
+	setPosition(Vector3::Zero);
+
+	// Sets the default volume.
+	setVolume(1.0f);
+}
+
+//-----------------------------------//
+
+AudioContext::~AudioContext()
+{
+	if(!context)
+		return;
+	
+	if(device->context == context)
+		device->context = nullptr;
+
+	alcDestroyContext(context);
+}
+
+//-----------------------------------//
+
+ALCcontext* AudioContext::createContext()
+{
+	ALCcontext* context = alcCreateContext(device->device, nullptr);
+
+	if(checkError())
+	{
+		#pragma TODO("OpenAL getError() should be context-specific")
+		Log::warn( getError().c_str() );
+	}
+
+	return context;
+}
+
+//-----------------------------------//
+
+void AudioContext::setVolume(float volume)
+{
+	// Update OpenAL's volume.
+	alListenerf(AL_GAIN, volume);
+
+	if(checkError())
+		Log::warn("Could not set new volume: %s", getError().c_str());
+}
+
+//-----------------------------------//
+
+void AudioContext::setPosition(const Vector3& position)
+{
+	// Update OpenAL's listener position.
+	alListenerfv(AL_POSITION, &position.x);
+
+	if(checkError()) 
+		Log::warn( "Could not set listener position: %s", getError().c_str());
+}
+
+//-----------------------------------//
+
+void AudioContext::setOrientation(const Vector3& rotation)
+{
+	Vector3 data[] = { rotation, Vector3::UnitY };
+
+	alListenerfv(AL_ORIENTATION, (const ALfloat *) &data );
+
+	if(checkError()) 
+		Log::warn( "Could not set listener orientation: %s", getError().c_str());
+}
+
+//-----------------------------------//
+
+void AudioContext::makeCurrent()
+{
+	device->switchContext(context);
+}
+
+//-----------------------------------//
+
+const std::string AudioContext::getError()
+{
+	const ALchar* str;
+	
+	switch (error)
+	{
+	case ALC_NO_ERROR:
+		str = "No error."; 
+		break;
+    case ALC_INVALID_DEVICE:
+        str = "Invalid device.";
+        break;
+    case ALC_INVALID_CONTEXT:
+        str = "Invalid context.";
+        break;
+	case ALC_INVALID_ENUM:
+		str = "Invalid enum.";
+		break;
+	case ALC_INVALID_VALUE:
+		str = "Invalid value.";
+		break;
+    case ALC_OUT_OF_MEMORY:
+        str = "Out of memory.";
+        break;
+	default:
+		str = "Unknown error.";
+		break;
+	}
+	
+	return str;
+}
+
+//-----------------------------------//
+
+bool AudioContext::checkError()
+{
+	error = alcGetError(device->device);
+	return (error != AL_NO_ERROR);
+}
+
+//-----------------------------------//
+
+} // end namespace
+
+#endif
---
+++ src/Engine/Source.cpp
@@ -0,0 +1,205 @@
+/************************************************************************
+*
+* vapor3D Engine © (2008-2010)
+*
+*	<http://www.vapor3d.org>
+*
+************************************************************************/
+
+#include "vapor/PCH.h"
+
+#ifdef VAPOR_AUDIO_OPENAL
+
+#include "scene/Source.h"
+#include "audio/Source.h"
+#include "resources/Sound.h"
+#include "Engine.h"
+
+namespace vapor {
+
+//-----------------------------------//
+
+BEGIN_ENUM(SourceState)
+	ENUM(Play)
+	ENUM(Pause)
+	ENUM(Stop)
+END_ENUM()
+
+BEGIN_ENUM(SourceMode)
+	ENUM(Static)
+	ENUM(Streaming)
+END_ENUM()
+
+BEGIN_CLASS_PARENT(Source, Component)
+	FIELD_ENUM_SETTER(SourceState, state, State)
+	FIELD_ENUM_SETTER(SourceMode, mode, Mode)
+	FIELD_PRIMITIVE_SETTER(bool, loop, Loop)
+	FIELD_PRIMITIVE_SETTER(float, volume, Volume)
+	FIELD_PRIMITIVE_SETTER(float, pitch, Pitch)
+	FIELD_PRIMITIVE_SETTER(float, minDistance, MinDistance)
+	FIELD_PRIMITIVE_SETTER(float, maxDistance, MaxDistance)
+	FIELD_ENUM_SETTER(RolloffMode, rolloffMode, RolloffMode)
+	FIELD_PRIMITIVE_SETTER(float, rolloff, Rolloff)
+	FIELD_CLASS_PTR_SETTER(Sound, SoundPtr, sound, RefPointer, Sound)
+END_CLASS()
+
+//-----------------------------------//
+
+Source::Source()
+	: state(SourceState::Stop)
+	, mode(SourceMode::Static)
+	, volume(1.0f)
+	, pitch(1.0f)
+	, minDistance(20.0f)
+	, maxDistance(40.0f)
+	, rolloffMode(RolloffMode::Logarithmic)
+	, rolloff(1.0f)
+	, loop(false)
+	, sound(nullptr)
+	, audioSource(nullptr)
+{ }
+
+//-----------------------------------//
+
+Source::~Source()
+{
+	delete audioSource;
+}
+
+//-----------------------------------//
+
+void Source::setVolume(float volume)
+{
+	this->volume = volume;
+
+	if(audioSource)
+		audioSource->setVolume(volume);
+}
+
+//-----------------------------------//
+
+void Source::setPitch(float pitch)
+{
+	this->pitch = pitch;
+
+	if(audioSource)
+		audioSource->setPitch(pitch);
+}
+
+//-----------------------------------//
+
+void Source::setMinDistance( float distance )
+{
+	this->minDistance = distance;
+
+	if(audioSource)
+		audioSource->setReferenceDistance(distance);
+}
+
+//-----------------------------------//
+
+void Source::setMaxDistance( float distance )
+{
+	this->maxDistance = distance;
+
+	if(audioSource)
+		audioSource->setMaxDistance(distance);
+}
+
+//-----------------------------------//
+
+void Source::setRolloff(float rolloff)
+{
+	this->rolloff = rolloff;
+
+	if(audioSource)
+		audioSource->setRolloff(rolloff);
+}
+
+//-----------------------------------//
+
+void Source::setRolloffMode(RolloffMode::Enum mode)
+{
+	this->rolloffMode = mode;
+
+	if(audioSource)
+		audioSource->setRolloffMode(rolloffMode);
+}
+
+//-----------------------------------//
+
+void Source::setLoop(bool state)
+{
+	this->loop = state;
+
+	if(audioSource)
+		audioSource->setLoop(state);
+}
+
+//-----------------------------------//
+
+void Source::setState( SourceState::Enum state )
+{
+	this->state = state;
+
+	if(!audioSource)
+		return;
+
+	if(state == SourceState::Play && !audioSource->isPlaying())
+		audioSource->play();
+	
+	else if(state == SourceState::Pause)
+		audioSource->pause();
+
+	else if(state == SourceState::Stop)
+		audioSource->stop();
+}
+
+//-----------------------------------//
+
+void Source::setMode( SourceMode::Enum mode )
+{
+	this->mode = mode;
+
+	if(!audioSource)
+		return;
+}
+
+//-----------------------------------//
+
+void Source::setSound( const SoundPtr& sound )
+{
+	this->sound = sound;
+
+	AudioDevice* device = GetEngine()->getAudioDevice();
+	AudioContext* context = device->getMainContext();
+
+	audioSource = new AudioSource(context, sound);
+
+	setVolume(volume);
+	setPitch(pitch);
+	setRolloff(rolloff);
+	setMode(mode);
+	setState(state);
+}
+
+//-----------------------------------//
+
+void Source::update( double delta )
+{
+	if( !audioSource )
+		return;
+
+	if( audioSource->isPlaying() )
+		state = SourceState::Play;
+	else if( audioSource->isPaused() )
+		state = SourceState::Pause;
+	else
+		state = SourceState::Stop;
+}
+
+//-----------------------------------//
+
+} // end namespace
+
+#endif
