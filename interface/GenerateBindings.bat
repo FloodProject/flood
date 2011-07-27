b17cd19dd9ef24f08fff8da2c4f4e6e7b843464d
João Matos <triton@vapor3d.org>
2011-07-27 16:39:18 +0000

    Bindings:  - Reorganized and renamed the interface files.

--- interface/GenerateBindings.bat
+++ interface/GenerateBindings.bat
@@ -1,2 +1,2 @@
-"swigwin-2.0.1\swig.exe" -v -debug-classes -c++ -lua -I"..\inc\vapor" -I"..\inc" -I"..\src" vapor.i
+"swigwin-2.0.1\swig.exe" -v -debug-classes -c++ -lua -I"..\inc\vapor" -I"..\inc" -I"..\src" Bindings\Engine.i
 "swigwin-2.0.1\swig.exe" -c++ -lua -external-runtime
