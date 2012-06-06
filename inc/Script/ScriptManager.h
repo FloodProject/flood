eb453f0da4724afe2c0027ab74ba522845836a8e
João Matos <triton@vapor3d.org>
2012-06-06 18:32:12 +0000

    Removed some Lua-specific scripting code.

--- inc/Script/ScriptManager.h
+++ inc/Script/ScriptManager.h
@@ -12,14 +12,7 @@
 #include "Script/State.h"
 #include "Resources/ResourceManager.h"
 
-#ifdef ENABLE_SCRIPTING_LUA
-
 NAMESPACE_ENGINE_BEGIN
-
-//-----------------------------------//
-
-typedef std::map<State*, Script*> ScriptsMap;
-typedef std::pair<const State*, Script> ScriptsMapPair;
 
 //-----------------------------------//
 
@@ -34,12 +27,6 @@ public:
 	ScriptManager();
 	~ScriptManager();
 
-	// Creates a new script instance.
-	State* createScriptInstance(Script* script);
-
-	// Gets the Lua state.
-	GETTER(State, State*, state)
-
 	// Updates all the registered scripts.
 	void update() OVERRIDE;
 
@@ -47,16 +34,8 @@ protected:
 
 	// Gets called when a resource is reloaded.
 	void onReload( const ResourceEvent& event );
-
-	// Lua VM state.
-	State* state;
-
-	// Holds Lua script instances.
-	ScriptsMap scripts;
 };
 
 //-----------------------------------//
 
 NAMESPACE_ENGINE_END
-
-#endif
--- src/Engine/ScriptManager.cpp
+++ src/Engine/ScriptManager.cpp
@@ -8,40 +8,17 @@
 
 #include "Engine/API.h"
 
-#ifdef ENABLE_SCRIPTING_LUA
-
 #include "Script/ScriptManager.h"
 #include "Resources/ResourceManager.h"
-
-#include <lua.hpp>
-
-extern "C" int luaopen_vapor(lua_State* L);
 
 NAMESPACE_ENGINE_BEGIN
 
 //-----------------------------------//
 
 ScriptManager::ScriptManager()
-	: state(nullptr)
 {
 	ResourceManager* res = GetResourceManager();
 	res->onResourceReloaded.Connect( this, &ScriptManager::onReload );
-
-	// Create a new Lua VM instance.
-	state = AllocateHeap(State, luaL_newstate());
-
-	// Check for proper initialization of the Lua state.
-	if( !state->getLuaState() )
-	{
-		LogError( "Error initializing %s", LUA_RELEASE );
-		return;
-	}
-
-	// Initialize the standard libraries (we want to disallow I/O though).
-	luaL_openlibs( state->getLuaState() );
-	//luaopen_vapor( state->getLuaState() );
-
-	LogInfo( "Initialized %s", LUA_RELEASE );
 }
 
 //-----------------------------------//
@@ -50,53 +27,13 @@ ScriptManager::~ScriptManager()
 {
 	ResourceManager* res = GetResourceManager();
 	res->onResourceReloaded.Disconnect( this, &ScriptManager::onReload );
-
-	LogInfo("Cleaning up the Lua state");
-	Deallocate(state);
-}
-
-//-----------------------------------//
-
-State* ScriptManager::createScriptInstance(Script* script)
-{
-	if( !script ) return nullptr;
-
-	// Create a new thread state sharing the main state.
-	lua_State* thread = lua_newthread( state->getLuaState() );
-	State* newState = AllocateHeap(State, thread);
-
-	lua_State* L = thread;
-
-	// Replaces the environment table of the thread.
-	lua_pushthread(L);                  // thread
-	lua_newtable(L);                    // thread, t
-	lua_pushvalue(L, LUA_GLOBALSINDEX); // thread, t, _G
-	lua_setfield(L, -2, "__index");     // thread, t
-	lua_pushvalue(L, -1);               // thread, t, t
-	lua_setmetatable(L, -2);            // thread, t
-	lua_setfenv(L, -2);                 // thread 
-
-	// Keeps a reference to the thread in the registry.
-    //int top = lua_gettop(L);
-    //lua_getfield(L, LUA_REGISTRYINDEX, "Scripts"); // top + 1
-    //lua_pushlightuserdata(L, newState); // key, the pointer to my own Script class
-    //lua_pushvalue(L, top+2);          // value, the new lua thread.
-    //lua_rawset(L, top+1); // Scripts table
-    //lua_settop(L, top); // Cleans the stack.
-
-	// Register the new state.
-	scripts[newState] = script;
-
-	// Register the source in the state.
-	newState->load( script );
-	
-	return newState;
 }
 
 //-----------------------------------//
 
 void ScriptManager::onReload( const ResourceEvent& event )
 {
+#if 0
 	const ScriptHandle& handle = HandleCast<Script>(event.handle);
 	Script* script = handle.Resolve();
 
@@ -116,6 +53,7 @@ void ScriptManager::onReload( const ResourceEvent& event )
 		
 		state->load( script );
 	}
+#endif
 }
 
 //-----------------------------------//
@@ -128,5 +66,3 @@ void ScriptManager::update()
 //-----------------------------------//
 
 NAMESPACE_ENGINE_END
-
-#endif
