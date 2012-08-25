926cb42f4bdb307ff196a17b6895cafc65fa89b7
triton <ripzonetriton@gmail.com>
2012-08-25 04:13:20 +0100

    Added Mono dependency build file.

---
+++ deps/Mono/premake4.lua
@@ -0,0 +1,179 @@
+project "Mono"
+	
+	local version = "2.12"
+	local repo = "git|git://github.com/mono/mono.git"
+	local license = "LGPL"
+	
+	kind "StaticLib"
+	
+	defines
+	{
+		"__default_codegen__",
+		"HAVE_CONFIG_H",
+	}
+	
+	-- sgen defines
+	defines
+	{
+		"HAVE_SGEN_GC",
+		"HAVE_MOVING_COLLECTOR",
+		"HAVE_WRITE_BARRIERS"
+	}
+	
+	includedirs
+	{
+		"./",
+		"mono/",
+		"eglib/src/"
+	}
+	
+	files
+	{
+		"eglib/src/*.c",
+		"mono/metadata/*.c",
+		"mono/mini/*.c",
+		"mono/utils/*.c",
+	}
+	
+	excludes
+	{	
+		"mono/metadata/boehm-gc.c",
+		
+		"mono/metadata/console-unix.c",
+		"mono/metadata/console-win32.c",
+		
+		--"mono/metadata/coree.c",
+		"mono/metadata/monodiet.c",
+		"mono/metadata/monosn.c",
+		"mono/metadata/null-gc.c",
+		"mono/metadata/pedump.c",
+		"mono/metadata/tpool-*.c",
+		"mono/metadata/sgen-fin-weak-hash.c",
+		
+		"mono/mini/fsacheck.c",
+		"mono/mini/genmdesc.c",
+		"mono/mini/main.c",
+		
+		"mono/mini/mini-darwin.c",
+		
+		"mono/mini/mini-alpha.c",
+		"mono/mini/exceptions-alpha.c",
+		"mono/mini/tramp-alpha.c",
+		
+		"mono/mini/mini-arm.c",
+		"mono/mini/exceptions-arm.c",
+		"mono/mini/tramp-arm.c",
+		
+		"mono/mini/mini-hppa.c",
+		"mono/mini/exceptions-hppa.c",
+		"mono/mini/tramp-hppa.c",		
+		
+		"mono/mini/mini-ia64.c",
+		"mono/mini/exceptions-ia64.c",
+		"mono/mini/tramp-ia64.c",
+		
+		"mono/mini/mini-mips.c",
+		"mono/mini/exceptions-mips.c",
+		"mono/mini/tramp-mips.c",
+		
+		"mono/mini/mini-ppc.c",
+		"mono/mini/exceptions-ppc.c",
+		"mono/mini/tramp-ppc.c",
+		
+		"mono/mini/mini-s390*.c",
+		"mono/mini/exceptions-s390*.c",
+		"mono/mini/tramp-s390*.c",
+		
+		"mono/mini/mini-sparc.c",
+		"mono/mini/exceptions-sparc.c",
+		"mono/mini/tramp-sparc.c",		
+		
+		"mono/mini/mini-llvm.c",
+		"mono/mini/mini-posix.c",
+
+		"mono/utils/mono-embed.c",
+	}
+	
+	configuration "not windows"
+		excludes { "eglib/src/*-win32.c" }
+		files { "eglib/src/*-unix.c" }
+	
+	configuration "not x32"
+		excludes
+		{
+			--"mono/mini/mini-x86.c",
+			--"mono/mini/exceptions-x86.c",
+			--"mono/mini/tramp-x86.c",
+		}
+		files
+		{
+			--"mono/mini/mini-amd64.c",
+			--"mono/mini/exceptions-amd64.c",
+			--"mono/mini/tramp-amd64.c",		
+		}
+		
+	configuration "not x64"
+		excludes
+		{
+			"mono/mini/mini-amd64.c",
+			"mono/mini/exceptions-amd64.c",
+			"mono/mini/tramp-amd64.c",
+		}
+		files
+		{
+			"mono/mini/mini-x86.c",
+			"mono/mini/exceptions-x86.c",
+			"mono/mini/tramp-x86.c",	
+		}
+			
+	configuration "windows"
+		files
+		{
+			"mono/mini/mini-windows.c"
+		}
+		excludes
+		{
+			"eglib/src/*-unix.c"
+		}
+		defines
+		{
+			"WIN32_THREADS",
+			"WINVER=0x0500",
+			"_WIN32_WINNT=0x0500",
+			"_WIN32_IE=0x0501",
+			"_UNICODE", "UNICODE",
+			"FD_SETSIZE=1024"
+		}
+		links
+		{
+			"Mswsock",
+			"ws2_32",
+			"psapi",
+			"version",
+			"winmm"
+		}
+		
+	configuration "linux"
+		files
+		{
+			"mono/metadata/tpool-epoll.c"
+		}
+		
+	configuration "macosx or freebsd"
+		files
+		{
+			"mono/metadata/tpool-kqueue.c"
+		}		
+	
+	configuration "vs*"
+		defines { "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE" }
+		buildoptions
+		{
+			"/wd4018", -- signed/unsigned mismatch
+			"/wd4244", -- conversion from 'x' to 'y', possible loss of data
+			"/wd4133", -- incompatible types - from 'x *' to 'y *'
+			"/wd4715", -- not all control paths return a value
+			"/wd4047", -- 'x' differs in levels of indirection from 'y'
+		}
+	
+	configuration ""
