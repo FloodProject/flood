5f69ae376eaed89e9cf28795906837d84f4f57f3
triton <ripzonetriton@gmail.com>
2012-08-25 04:39:51 +0100

    Updated the stb dependencies.

--- deps/SeanBarrett/premake4.lua
+++ deps/SeanBarrett/premake4.lua
@@ -1,9 +1,26 @@
-project "STBImage"
+project "stb_image"
 		
 	local version = "1.33"
+	local license = "Public Domain"
+	local archive = "http://nothings.org/stb_vorbis/stb_vorbis.c"
+	
+  kind "StaticLib"
+  files { "stb_image.c" }
+
+project "stb_vorbis"
+		
+	local version = "0.99996"
 	local license = "Public Domain"
 	local archive = "http://nothings.org/stb_image.c"
 	
   kind "StaticLib"
-  files { "stb_image.c" }
- 
+  files { "stb_vorbis.c" }
+
+ project "stb_truetype"
+		
+	local version = "0.6"
+	local license = "Public Domain"
+	local archive = "http://nothings.org/stb/stb_truetype.h"
+	
+  kind "StaticLib"
+  files { "stb_truetype.c" }
