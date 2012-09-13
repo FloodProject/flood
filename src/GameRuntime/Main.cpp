bf494e070bb2901591ba874f6dd0479c35b0f395
chart <the.corbin.hart@gmail.com>
2012-09-13 15:25:57 -0500

    Added initial implementation of the Game class. * wrapper around getting a real-time simulation running * inits the engine, creates a window, executes the game loop
    **NOTE:** much like the editor, the Game contains the Engine instance. To access this from the Game, use the global game() function, ex. game()->engine->init() etc. Ideally, when we fix the namespaces, it would be nicer to be more pedantic about this, perhaps: fl::game()->whatever() etc.

---
+++ src/GameRuntime/Main.cpp
@@ -0,0 +1,22 @@
+/************************************************************************
+*
+* Flood Project © (2008-201x)
+* Licensed under the simplified BSD license. All rights reserved.
+*
+************************************************************************/
+
+#include "GameRuntime/API.h"
+#include "GameRuntime/Game.h"
+
+//-----------------------------------//
+
+int main(int argc, char** argv)
+{
+	Game gameInstance;
+	
+	game()->run();
+
+	return 0;
+}
+
+
