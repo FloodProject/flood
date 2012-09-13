1bdaf96d88f621bbb7537574359ac76514d0e4ab
chart <the.corbin.hart@gmail.com>
2012-09-13 16:41:30 -0500

    Updated file header comments

--- src/Platforms/SFML/SFML_Window.cpp
+++ src/Platforms/SFML/SFML_Window.cpp
@@ -1,8 +1,7 @@
 /************************************************************************
 *
-* vapor3D Engine © (2008-2010)
-*
-*	<http://www.vapor3d.org>
+* Flood Project © (2008-201x)
+* Licensed under the simplified BSD license. All rights reserved.
 *
 ************************************************************************/
 
@@ -113,7 +112,7 @@ void SFML_Window::show( bool visible )
 
 void SFML_Window::makeCurrent()
 {
-	window.setActive(); 
+	window.setActive();
 }
 
 //-----------------------------------//
