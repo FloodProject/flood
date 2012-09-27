1263d27c600d084849df32ce85c2877cbc1101f5
triton <ripzonetriton@gmail.com>
2012-09-27 23:20:34 +0100

    Removed the old constructors of RenderState.

--- src/Graphics/RenderQueue.cpp
+++ src/Graphics/RenderQueue.cpp
@@ -14,25 +14,7 @@ NAMESPACE_GRAPHICS_BEGIN
 //-----------------------------------//
 
 RenderState::RenderState()
-{
-}
-
-//-----------------------------------//
-
-RenderState::RenderState(RenderBatch* batch)
-	: renderable( batch )
-	, material( batch->getMaterial().Resolve() )
-	, priority( batch->getRenderPriority() )
-{
-}
-
-//-----------------------------------//
-
-RenderState::RenderState(const RenderState& rhs)
-	: renderable( rhs.renderable )
-	, modelMatrix( rhs.modelMatrix )
-	, material( rhs.material )
-	, priority( rhs.priority )
+	: RenderCommand(RenderCommandType::RenderState)
 {
 }
 
