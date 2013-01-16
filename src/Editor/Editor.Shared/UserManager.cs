0671ea0784b93556403638d6f278a85620ad682d
triton <ripzonetriton@gmail.com>
2013-01-16 13:47:22 +0000

    Added start of the user manager interface.

---
+++ src/Editor/Editor.Shared/UserManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Flood.RPC.Metadata;
+
+namespace Flood.Editor
+{
+    [Message]
+    public struct UserId
+    {
+        [Id(0)]
+        public long Id;
+    }
+
+    public enum UserAuthType
+    {
+        None,
+        Password,
+        Certificate
+    }
+
+    public enum UserMessageIds
+    {
+        Auth,
+        AuthStatus,
+        ChangeName,
+        Join,
+        Leave,
+        NotifyName,
+        GetUserInfo,
+    }
+
+    public struct UserAuth
+    {
+        public string Name;
+        public UserAuthType AuthType;
+    }
+
+    public struct UserAuthStatus
+    {
+        public bool Status;
+    }
+
+    public struct UserJoin
+    {
+        public UserId User;
+        public string Name;
+    }
+
+    public struct UserLeave
+    {
+        
+    }
+
+    [Message]
+    public struct UserInfo
+    {
+        
+    }
+
+    [Service]
+    public interface IUser
+    {
+        bool Auth();
+        bool GetAuthStatus();
+        UserInfo GetInfo();
+    }
+
+    [Service]
+    public interface IUserManager
+    {
+        bool Auth([Id(0)] UserId user);
+
+        /// <summary>
+        /// Event fired when a new user is created.
+        /// </summary>
+        event EventHandler<IUser> UserCreated;
+    }
+}
