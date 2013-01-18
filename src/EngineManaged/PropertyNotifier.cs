fa9382c88a5907c2ab09d34afc81a70922ff651f
marcos henrich <marcoshenrich@hotmail.com>
2013-01-18 10:21:00 +0000

    Added missing EngineManaged PropertyNotifier.cs

---
+++ src/EngineManaged/PropertyNotifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineManaged
+{
+    public class PropertyAttribute : Attribute
+    {
+        
+    }
+
+
+    public delegate bool PropertyChanged(object obj, string propertyName, object oldValue, object newValue);
+
+    public interface IPropertyNotifier
+    {
+        event PropertyChanged PropertyChanged;
+    }
+}
