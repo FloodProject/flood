0d3656661d0eba2d3a391aadb8fccadf0824394e
João Mares <joaomares@gmail.com>
2013-02-08 01:26:40 +0000

    Generator tests added

---
+++ src/Tools/RPCGen/Tests/GeneratorTests.cs
@@ -0,0 +1,33 @@
+﻿using Flood.RPC.Protocol;
+using NUnit.Framework;
+using NSubstitute;
+using Flood.Tools.RPCGen;
+
+namespace RPCGen.Tests
+{
+    public class TestClass
+    {
+    }
+
+    [TestFixture]
+    public class GeneratorTests
+    {
+        private Generator generator;
+        private bool eventFired;
+
+        [SetUp]
+        public void Init()
+        {
+            Options options = new Options();
+            generator = new Generator(options);
+            eventFired = false;
+        }
+
+        [Test]
+        public void ProjectProperties()
+        {
+            TType type = Generator.ConvertFromTypeToThrift(typeof(TestClass));
+            Assert.AreEqual(TType.Struct, type) ;
+        }
+    }
+}
