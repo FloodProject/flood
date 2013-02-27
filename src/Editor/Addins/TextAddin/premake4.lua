8701180c20c776597ee9b26c7d2c0c7e0a58d1ef
marcos henrich <marcoshenrich@hotmail.com>
2013-02-27 19:11:42 +0000

    Added dummy addin, that adds tools with icons to toolbar

---
+++ src/Editor/Addins/TextAddin/premake4.lua
@@ -0,0 +1,34 @@
+EditorClient = {}
+EditorClient.name = "TextAddin"
+
+project "TextAddin"
+
+    SetupAddin()
+
+	kind "SharedLib"
+	language "C#"
+	
+	location "."
+	
+	files
+	{
+		"premake4.lua",
+		"**.cs",
+		"**.addin.xml",
+        path.join("icons","*.png")
+	}
+	
+	links
+	{
+		"System",
+		"EngineManaged",
+		"Editor.Client",
+        "Mono.Addins"
+	}
+    
+    linksnotcopied
+    {
+        "EngineManaged",
+		"Editor.Client",
+        "Mono.Addins"
+    }
