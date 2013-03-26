5cd0ced8a2f1ad93a0e33b9f2a6f183daf782c44
marcos henrich <marcoshenrich@hotmail.com>
2013-03-26 15:13:50 +0000

    Added missing header.

--- src/EditorRuntime/Managed/GwenRenderer.cpp
+++ src/EditorRuntime/Managed/GwenRenderer.cpp
@@ -1,3 +1,9 @@
+/************************************************************************
+*
+* Flood Project © (2008-201x)
+* Licensed under the simplified BSD license. All rights reserved.
+*
+************************************************************************/
 
 #include "Editor/API.h"
 #include "CLIInterop.h"
