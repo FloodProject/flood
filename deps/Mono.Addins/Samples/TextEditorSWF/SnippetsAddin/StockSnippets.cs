ef2a9be2dc08c6a914b23d7935318c8ce58cbe46
marcos henrich <marcoshenrich@hotmail.com>
2013-05-10 00:38:08 +0100

    Removed Mono.Addins files

--- deps/Mono.Addins/Samples/TextEditorSWF/SnippetsAddin/StockSnippets.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-using SnippetsAddin;
-
-[assembly: Snippet ("for", "for (int n=0; n<len; n++)\n{\n\t<|>\n}")]
-[assembly: Snippet ("foreach", "foreach (var item in col)\n{\n\t<|>\n}")]
