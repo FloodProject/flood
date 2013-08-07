5ae907a3fbc78b22c4c8f2143821de38060ee935
marcos henrich <marcoshenrich@hotmail.com>
2013-08-07 13:34:33 +0100

    Updated RPCGen.Tests.

--- src/Tools/RPCGen.Tests/Services/Test.cs
+++ src/Tools/RPCGen.Tests/Services/Test.cs
@@ -1,15 +1,10 @@
-﻿using EngineManaged.Network;
+﻿using Flood;
 using Flood.RPC;
 using Flood.RPC.Protocol;
-using Flood.RPC.Transport;
 using Flood.RPC.Metadata;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
@@ -50,14 +45,18 @@ namespace RPCGen.Tests.Services
         }
     }
 
+    public class ServiceManager : IRPCManager
+    {
+        public Processor Processor { get; set; }
+
+        public Task<RPCData> RemoteProcedureCall(RPCData data)
+        {
+            return Processor.Process(data);
+        }
+    }
+
     public class Tests
     {
-        struct ProcessInfo
-        {
-            public Processor Processor;
-            public Serializer Serializer;
-        }
-
         public void Test()
         {
             var implType = Assembly.GetExecutingAssembly().GetType(typeof(IService).FullName+"Impl");
@@ -65,31 +64,20 @@ namespace RPCGen.Tests.Services
 
             var clientType = implType.GetNestedType("Client");
             Assert.NotNull(clientType);
-            var clientCttr = clientType.GetConstructor(new Type[]{typeof(Serializer)});
+            var clientCttr = clientType.GetConstructor(new[]{typeof(IRPCManager), typeof(Session), typeof(int)});
             Assert.NotNull(clientCttr);
 
             var processorType = implType.GetNestedType("Processor");
             Assert.NotNull(processorType);
-            var processorCttr = processorType.GetConstructor(new Type[]{typeof(IService)});
+            var processorCttr = processorType.GetConstructor(new[]{typeof(IService)});
             Assert.NotNull(processorCttr);
 
-            var stream1 = new ProducerConsumerStream();
-            var stream2 = new ProducerConsumerStream();
-
-            var proxyTransport = new TStreamTransport(stream1, stream2);
-            var implTransport = new TStreamTransport(stream2, stream1);
-
-            var proxySerializer = new BinarySerializer(proxyTransport);
-            var implSerializer = new BinarySerializer(implTransport);
+            var serviceManager = new ServiceManager();
 
             var serviceImpl = new Service();
-            var serviceProxy = (IService) clientCttr.Invoke(new object[]{proxySerializer});
-            var processor = (Processor) processorCttr.Invoke(new object[]{serviceImpl});
+            var serviceProxy = (IService) clientCttr.Invoke(new object[]{serviceManager, null, 0});
+            serviceManager.Processor = (Processor) processorCttr.Invoke(new object[]{serviceImpl});
 
-            var thread = new Thread(new ParameterizedThreadStart(Process));
-            var processInfo = new ProcessInfo(){Processor=processor, Serializer=implSerializer};
-            thread.Start(processInfo);
-             
             var pingTask = serviceProxy.Ping();
             Assert.IsTrue(pingTask.Wait(1000));
             Assert.AreEqual("pong", pingTask.Result);
@@ -98,25 +86,6 @@ namespace RPCGen.Tests.Services
             Assert.IsTrue(messageTask.Wait(1000));
             Assert.AreEqual(1337, messageTask.Result.i);
             Assert.AreEqual("leet", messageTask.Result.s);
-
-            thread.Abort();
-        }
-
-        private void Process(object processInfo)
-        {
-            var p = (ProcessInfo)processInfo;
-            while (true)
-            {
-                try
-                {
-                    p.Processor.Process(p.Serializer, p.Serializer);
-                    Thread.Sleep(50);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-            }
         }
     }
 }
