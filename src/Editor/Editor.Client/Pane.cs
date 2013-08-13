4735ae31af498233a9dd9c5b720f16826d2d0f7c
marcos henrich <marcoshenrich@hotmail.com>
2013-08-13 23:19:47 +0100

    Renamed Panes.cs to Pane.cs and moved it from the extension folder to the project root directory.

---
+++ src/Editor/Editor.Client/Pane.cs
@@ -0,0 +1,17 @@
+﻿using Flood.GUI.Controls;
+
+namespace Flood.Editor.Client.GUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Pane 
+    {
+        public string Title = "Pane";
+
+        public virtual Control CreateControl(Control parent)
+        {
+            return new Button(parent);
+        }
+    }
+}
