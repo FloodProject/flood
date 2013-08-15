1d7e75432473aa0af628e29356d5ecf8eb5fe708
marcos henrich <marcoshenrich@hotmail.com>
2013-08-15 21:29:03 +0100

    Added ImplName helper function to Flood.RPC.

---
+++ src/EngineManaged/RPC/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flood.RPC
+{
+    public class Helper
+    {
+        public static string ImplName(Type type, bool fullName)
+        {
+            var name = type.FullName;
+            if (!fullName && name.Contains('.'))
+            {
+                var names = name.Split('.');
+                name = names[names.Length - 1];
+            }
+
+            return name.Replace("+", "_") + "Impl";
+        }
+    }
+}
