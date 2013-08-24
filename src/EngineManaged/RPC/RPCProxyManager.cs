8c169f535cef550a4086c9a0073449f56c4b24e6
marcos henrich <marcoshenrich@hotmail.com>
2013-08-24 11:04:06 +0100

    Removed Process(RPCData call) from RPCImplManager and RPCProxyManager, noe RPCMAnager directly calls RPCImpl or RPCProxy Process methods.

--- src/EngineManaged/RPC/RPCProxyManager.cs
+++ src/EngineManaged/RPC/RPCProxyManager.cs
@@ -8,33 +8,14 @@ namespace Flood.RPC
         private Dictionary<int, RPCProxy> proxies;
         private int proxyCounter;
 
-        private int sequenceNumber;
-
         public RPCProxyManager()
         {
             proxies = new Dictionary<int, RPCProxy>();
         }
 
-        internal void Process(RPCData data)
+        public RPCProxy this[int id]
         {
-            RPCProxy proxy;
-            if (!proxies.TryGetValue(data.Header.ProxyId, out proxy))
-            {
-                Log.Error("Received RPC data to unexpected Proxy.");
-                return;
-            }
-
-            switch (data.Header.CallType)
-            {
-            case RPCDataType.Reply:
-                proxy.ProcessReply(data);
-                return;
-            case RPCDataType.EventInvoke:
-                proxy.InvokeEvent(data);
-                return;
-            default:
-                throw new NotImplementedException();
-            }
+            get { return proxies[id]; }
         }
 
         internal T CreateProxy<T>(RPCPeer peer, int implId)
@@ -42,7 +23,7 @@ namespace Flood.RPC
             var serviceType = typeof(T);
             var serviceAssembly = serviceType.Assembly;
 
-            var implType = serviceAssembly.GetType(Helper.ImplName(serviceType, true));
+            var implType = serviceAssembly.GetType(Helper.GetStubsClassName(serviceType, true));
             var proxyType = implType.GetNestedType("Proxy");
 
             var proxyId = proxyCounter++;
