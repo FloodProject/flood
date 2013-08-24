18a6219ed5756c0fee23c6e301bc99e052c80785
marcos henrich <marcoshenrich@hotmail.com>
2013-08-24 11:05:32 +0100

    Added RPCDelegate.

---
+++ src/EngineManaged/RPC/RPCDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flood.RPC
+{
+    public abstract class RPCDelegate
+    {
+        protected RPCPeer Peer;
+        protected int ImplId;
+        protected int ProxyId;
+        protected int DelegateId;
+
+        protected Delegate Delegate;
+
+        public RPCDelegate(RPCPeer peer, int implId, int proxyId, int delegateId, Delegate del)
+        {
+            Peer = peer;
+            ImplId = implId;
+            ProxyId = proxyId;
+            DelegateId = delegateId;
+            Delegate = del;
+        }
+
+        public abstract void Invoke(RPCData data);
+    }
+}
