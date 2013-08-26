9d38d6356ddc0d72b1861ba50d65cc0f4c40782d
marcos henrich <marcoshenrich@hotmail.com>
2013-08-26 13:42:11 +0100

    Removed all RPCGen.Tests tests, and added simple RPCGen.Program.Main test.

---
+++ src/Tools/RPCGen.Tests/RPCGenTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace RPCGen.Tests
+{
+    [TestFixture]
+    class RPCGenTests 
+    {
+        [Test]
+        public void MainTest()
+        {
+            string genDirectory = Path.Combine("..", "..", "gen", "RPCGen.Tests");
+            Directory.CreateDirectory(genDirectory);
+
+            var sourceDllPath = Path.GetFullPath("RPCGen.Tests.Services.dll");
+            var destDllPath = Path.Combine(genDirectory, "RPCGen.Tests.Services.dll");
+            var sourcePdbPath = Path.GetFullPath("RPCGen.Tests.Services.pdb");
+            var destPdbPath = Path.Combine(genDirectory, "RPCGen.Tests.Services.pdb");
+
+            System.IO.File.Copy(sourceDllPath, destDllPath, true);
+
+            if (File.Exists(sourcePdbPath))
+                System.IO.File.Copy(sourcePdbPath, destPdbPath, true);
+
+            var args = new string[]
+            {
+                String.Format("-o={0}", genDirectory),
+                destDllPath
+            };
+
+            var ret = Flood.Tools.RPCGen.Program.Main(args);
+            Assert.AreEqual(0, ret);
+        }
+    }
+}
