2525526e922e8c7ab24d4e0d1e4a30489da86681
marcos henrich <marcoshenrich@hotmail.com>
2013-08-26 12:31:01 +0100

    Created RPCCallProcessor used by RPCProxy to dispatch RPCDataType.Call and receive RPCDataType.Reply, and by RPCDelegate to dispatch RPCDataType.DelegateCall and receive RPCDataType.DelegateReply.

---
+++ src/EngineManaged/RPC/RPCCallProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flood.RPC
+{
+    class RPCCallProcessor
+    {
+        private Dictionary<int, TaskCompletionSource<RPCData>> pendingCalls;
+        private int callIdCounter;
+
+        public RPCCallProcessor()
+        {
+            pendingCalls = new Dictionary<int, TaskCompletionSource<RPCData>>();
+        }
+
+        public RPCData.Call CreateCall(int methodId, RPCPeer peer, int localId, int remoteId, bool isDelegate)
+        {
+            var id = Interlocked.Increment(ref callIdCounter);
+            return new RPCData.Call(id, methodId, peer, localId, remoteId, isDelegate);
+        }
+
+        public Task<RPCData> DispatchCall(RPCData.Call call)
+        {
+            var tcs = new TaskCompletionSource<RPCData>();
+            pendingCalls.Add(call.Id, tcs);
+
+            call.Data.Peer.Dispatch(call.Data);
+
+            return tcs.Task;
+        }
+
+        internal void ProcessReply(RPCData.Reply reply)
+        {
+            if (!pendingCalls.ContainsKey(reply.Id))
+            {
+                Log.Error("Received unexpected reply.");
+                return;
+            }
+
+            pendingCalls[reply.Id].SetResult(reply.Data);
+        }
+    }
+}
