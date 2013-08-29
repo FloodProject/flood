ca639e09ca9db6ee318248ffecb4dd120de6a6cd
marcos henrich <marcoshenrich@hotmail.com>
2013-08-29 11:00:20 +0100

    Added FastInvoke class to EngineWeaver.

---
+++ src/Tools/EngineWeaver/Util/FastInvoke.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineWeaver.Util
+{
+    class FastInvoke
+    {
+        public delegate object FastInvokeHandler(object target, object[] paramters);
+
+        public static FastInvokeHandler GetMethodInvoker(MethodInfo methodInfo)
+        {
+            DynamicMethod dynamicMethod = new DynamicMethod(string.Empty, 
+                             typeof(object), new Type[] { typeof(object), 
+                             typeof(object[]) }, 
+                             methodInfo.DeclaringType.Module);
+            ILGenerator il = dynamicMethod.GetILGenerator();
+            ParameterInfo[] ps = methodInfo.GetParameters();
+            Type[] paramTypes = new Type[ps.Length];
+            for (int i = 0; i < paramTypes.Length; i++)
+            {
+                paramTypes[i] = ps[i].ParameterType;
+            }
+            LocalBuilder[] locals = new LocalBuilder[paramTypes.Length];
+            for (int i = 0; i < paramTypes.Length; i++)
+            {
+                locals[i] = il.DeclareLocal(paramTypes[i]);
+            }
+            for (int i = 0; i < paramTypes.Length; i++)
+            {
+                il.Emit(OpCodes.Ldarg_1);
+                EmitFastInt(il, i);
+                il.Emit(OpCodes.Ldelem_Ref);
+                EmitCastToReference(il, paramTypes[i]);
+                il.Emit(OpCodes.Stloc, locals[i]);
+            }
+            il.Emit(OpCodes.Ldarg_0);
+            for (int i = 0; i < paramTypes.Length; i++)
+            {
+                il.Emit(OpCodes.Ldloc, locals[i]);
+            }
+            il.EmitCall(OpCodes.Call, methodInfo, null);
+            if (methodInfo.ReturnType == typeof(void))
+                il.Emit(OpCodes.Ldnull);
+            else
+                EmitBoxIfNeeded(il, methodInfo.ReturnType);
+            il.Emit(OpCodes.Ret);
+            FastInvokeHandler invoder = 
+              (FastInvokeHandler)dynamicMethod.CreateDelegate(
+              typeof(FastInvokeHandler));
+            return invoder;
+        }
+
+        private static void EmitCastToReference(ILGenerator il, System.Type type)
+        {
+            if (type.IsValueType)
+            {
+                il.Emit(OpCodes.Unbox_Any, type);
+            }
+            else
+            {
+                il.Emit(OpCodes.Castclass, type);
+            }
+        }
+
+        private static void EmitBoxIfNeeded(ILGenerator il, System.Type type)
+        {
+            if (type.IsValueType)
+            {
+                il.Emit(OpCodes.Box, type);
+            }
+        }
+
+        private static void EmitFastInt(ILGenerator il, int value)
+        {
+            switch (value)
+            {
+                case -1:
+                    il.Emit(OpCodes.Ldc_I4_M1);
+                    return;
+                case 0:
+                    il.Emit(OpCodes.Ldc_I4_0);
+                    return;
+                case 1:
+                    il.Emit(OpCodes.Ldc_I4_1);
+                    return;
+                case 2:
+                    il.Emit(OpCodes.Ldc_I4_2);
+                    return;
+                case 3:
+                    il.Emit(OpCodes.Ldc_I4_3);
+                    return;
+                case 4:
+                    il.Emit(OpCodes.Ldc_I4_4);
+                    return;
+                case 5:
+                    il.Emit(OpCodes.Ldc_I4_5);
+                    return;
+                case 6:
+                    il.Emit(OpCodes.Ldc_I4_6);
+                    return;
+                case 7:
+                    il.Emit(OpCodes.Ldc_I4_7);
+                    return;
+                case 8:
+                    il.Emit(OpCodes.Ldc_I4_8);
+                    return;
+            }
+
+            if (value > -129 && value < 128)
+            {
+                il.Emit(OpCodes.Ldc_I4_S, (SByte)value);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldc_I4, value);
+            }
+        }
+    }
+}
