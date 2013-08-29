44bb1f0bc53ea6168278b02992a3365efcda2f65
marcos henrich <marcoshenrich@hotmail.com>
2013-08-29 11:48:48 +0100

    Added TemporaryAssemblyPaths class used for deleting assembly files on dispose.

---
+++ src/Tools/RPCGen/TemporaryAssemblyPaths.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.IO;
+
+namespace RPCGen
+{
+    public sealed class TemporaryAssemblyPaths : IDisposable 
+    {
+        public string DllPath { get; private set; }
+        public string PdbPath { get; private set; }
+
+        public TemporaryAssemblyPaths() :
+            this(Path.GetTempPath())
+        {
+        }
+
+        public TemporaryAssemblyPaths(string directory)
+        {
+            var randomFileName = Path.Combine(directory, Path.GetRandomFileName());
+            DllPath = randomFileName + ".dll";
+            PdbPath = randomFileName + ".pdb";
+        }
+
+        ~TemporaryAssemblyPaths() {
+            Delete();
+        }
+
+        public void Dispose() {
+            Delete();
+            GC.SuppressFinalize(this);
+        }
+
+        private void Delete() {
+            if (File.Exists(DllPath))
+                File.Delete(DllPath);
+            if (File.Exists(PdbPath))
+                File.Delete(PdbPath);
+        }
+    }
+}
