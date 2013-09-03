c549108eabf243db90ba8455b79f5150b6f87080
marcos henrich <marcoshenrich@hotmail.com>
2013-09-03 01:19:42 +0100

    Added to RPC.Serialization IDataObject.

---
+++ src/EngineManaged/RPC/Serialization/IDataObject.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Flood.RPC.Serialization
+{
+    public interface IDataObject
+    {
+        void Read(RPCData data);
+        void Write(RPCData data);
+        void Write(RPCData data, int[] propertyIds);
+    }
+}
