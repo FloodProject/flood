d2594426ca0118ed1e4e6763994a5d616afe4d14
marcos henrich <marcoshenrich@hotmail.com>
2013-09-14 21:26:20 +0100

    Added PackageManager.

---
+++ src/EngineManaged/Packages/PackageManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flood.Packages;
+using Flood.RPC;
+
+namespace Flood.Packages
+{
+    public class PackageManager
+    {
+        public readonly Dictionary<PackageName, IPackage> Packages;
+
+        private PackageLoader packageLoader;
+
+        public PackageManager(string packageFolder, RPCManager rpcManager)
+        {
+            Packages = new Dictionary<PackageName, IPackage>();
+            packageLoader = new PackageLoader(packageFolder, rpcManager);
+        }
+
+        public IPackage GetLoadPackage(PackageName name)
+        {
+            if (Packages.ContainsKey(name))
+                return Packages[name];
+
+            var package = packageLoader.LoadPackage(name);
+            Packages.Add(name, package);
+
+            return package;
+        }
+
+        public void UpdatePackages()
+        {
+            foreach (var package in Packages.Values)
+            {
+                try
+                {
+                   package.Update();
+                }
+                catch
+                {
+                    Log.Error("Error updating package.");
+                }
+            }
+        }
+    }
+}
