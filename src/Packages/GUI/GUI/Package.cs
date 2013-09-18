80f718ac15daf762d9d7b0fbe828a2a62cc1a34b
marcos henrich <marcoshenrich@hotmail.com>
2013-09-18 11:04:37 +0100

    Added GUI/GUI/Package.cs.

---
+++ src/Packages/GUI/GUI/Package.cs
@@ -0,0 +1,18 @@
+﻿using Flood.Packages;
+using Flood.RPC;
+
+namespace GUI
+{
+    public class Package : IPackage
+    {
+        public void OnLoad(RPCManager rpcManager)
+        {
+
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
