c7675d3d68fce2de44a0d7b592953138af78ccd8
marcos henrich <marcoshenrich@hotmail.com>
2013-09-18 10:05:39 +0100

    Moved StreamExtensions from Utils to Extensions.

--- src/EngineManaged/Extensions.cs
+++ src/EngineManaged/Extensions.cs
@@ -44,4 +44,61 @@ namespace Flood
         }
     }
 
+    internal static class StreamExtensions
+    {
+        /// <summary>
+        /// Reads the contents of the stream into a byte array.
+        /// data is returned as a byte array. An IOException is
+        /// thrown if any of the underlying IO calls fail.
+        /// </summary>
+        /// <param name="stream">The stream to read.</param>
+        /// <returns>A byte array containing the contents of the stream.</returns>
+        /// <exception cref="NotSupportedException">The stream does not support reading.</exception>
+        /// <exception cref="ObjectDisposedException">Methods were called after the stream was closed.</exception>
+        /// <exception cref="System.IO.IOException">An I/O error occurs.</exception>
+        public static byte[] ReadAllBytes(this System.IO.Stream source)
+        {
+            long originalPosition = source.Position;
+            source.Position = 0;
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = source.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = source.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            System.Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            System.Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    System.Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                source.Position = originalPosition;
+            }
+        }
+    }
+
 }
