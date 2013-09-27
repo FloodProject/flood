853b66a9657e52772e45c841ee565a86a5a2573e
marcos henrich <marcoshenrich@hotmail.com>
2013-09-27 16:19:01 +0100

    Add DelayedCopy class to Weaver.

---
+++ src/Tools/Weaver/Util/DelayedCopy.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Weaver.Util
+{
+    interface IDelayedCopy
+    {
+        void Copy();
+    }
+    class DelayedCopy<T> : IDelayedCopy
+    {
+        private readonly T originObject;
+        private readonly T destObject;
+        private readonly CopyDelegate action;
+
+        public delegate void CopyDelegate(T originObject, T destObject);
+
+        public DelayedCopy(T originObject, T destObject, CopyDelegate action)
+        {
+            this.originObject = originObject;
+            this.destObject = destObject;
+            this.action = action;
+        }
+
+        public void Copy()
+        {
+            action.Invoke(originObject, destObject);
+        }
+    }
+}
