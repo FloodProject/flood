ea6893e669ec8c1f7bc2f4f2df6bc9edfae2f74e
marcos henrich <marcoshenrich@hotmail.com>
2013-10-04 18:51:17 +0100

    Added window to EngineManaged.

---
+++ src/EngineManaged/Windows/Window.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Collections.Generic;
+using Flood.RPC.Metadata;
+
+namespace Flood.Windows
+{
+    [DataObject(6)]
+    public class WindowRenderable
+    {
+        public virtual void Init()
+        {
+        }
+
+        public virtual void Render(RenderBlock renderBlock)
+        {
+        }
+
+        public virtual void Update()
+        {
+        }
+
+        public virtual void Resize(int width, int height)
+        {
+        }
+    }
+
+    [DataObject(5)]
+    public class Window
+    {
+        private RenderDevice device;
+        private RenderView view;
+
+        [Id(1)]
+        public List<WindowRenderable> Renderables;
+
+        internal Window()
+        {
+            Renderables = new List<WindowRenderable>();
+        }
+
+        internal void Init(Flood.Window window, RenderDevice renderDevice)
+        {
+            device = renderDevice;
+
+            var context = window.CreateContext(new RenderContextSettings());
+
+            view = window.CreateView();
+            view.ClearColor = Color.Red;
+
+            window.Idle += Update;
+            window.Render += Render;
+            window.TargetResize += Resize;
+            window.Show(true);
+
+            window.MakeCurrent();
+            context.Init();
+        }
+
+        private void Update()
+        {
+            foreach (var appRenderable in Renderables)
+                appRenderable.Update();
+        }
+
+        public void Render()
+        {
+            var rb = new RenderBlock();
+
+            foreach (var appRenderable in Renderables)
+                appRenderable.Render(rb);
+
+            device.ActiveView = view;
+            device.ClearView();
+
+            device.Render(rb);
+            //Application.Instance.Engine.StepFrame();
+        }
+
+        public void Resize(Settings settings)
+        {
+            foreach (var appRenderable in Renderables)
+                appRenderable.Resize(settings.Width, settings.Height);
+        }
+    }
+}
---
+++ src/EngineManaged/Windows/WindowManager.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flood;
+using Flood.RPC.Metadata;
+
+namespace Flood.Windows
+{
+    [GlobalService(1)]
+    public interface IWindowManager
+    {
+        [Id(1)]
+        Task<Window> GetCreateWindow([Id(1)]string windowName);
+    }
+
+    class WindowManager : IWindowManager
+    {
+        private Dictionary<string, Window> windows;
+        private Application application;
+
+        public WindowManager(Application application)
+        {
+            this.application = application;
+            windows = new Dictionary<string, Window>();
+        }
+
+        public async Task<Window> GetCreateWindow(string windowName)
+        {
+            if (windows.ContainsKey(windowName))
+                return windows[windowName];
+
+            var window = new Window();
+            windows.Add(windowName, window);
+
+            var windowSettings = new WindowSettings(1000, 700, windowName, WindowStyles.TopLevel);
+            var nativeWindow = application.WindowManager.CreateWindow(windowSettings);
+            window.Init(nativeWindow, application.RenderDevice);
+
+            return window;
+        }
+    }
+}
