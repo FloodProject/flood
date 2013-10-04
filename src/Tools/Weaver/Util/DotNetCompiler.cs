171701999822995615e96d4ff81fa99d3e5a482c
marcos henrich <marcoshenrich@hotmail.com>
2013-10-04 18:39:07 +0100

    Added helper class DotNetCompiler to weaver.

---
+++ src/Tools/Weaver/Util/DotNetCompiler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace Weaver.Util
+{
+    public class DotNetCompiler
+    {
+        public static void CompileIntoAssembly(string outputPath, IEnumerable<string> references, List<string> files)
+        {
+            CodeDomProvider provider = new CSharpCodeProvider();
+            CompilerParameters cp = new CompilerParameters()
+            {
+                GenerateExecutable = false,
+                OutputAssembly = outputPath,
+                IncludeDebugInformation = true,
+                CompilerOptions = "/unsafe"
+            };
+
+            foreach (var @ref in references)
+                cp.ReferencedAssemblies.Add(@ref);
+
+            CompilerResults cr = provider.CompileAssemblyFromFile(cp, files.ToArray());
+
+            if (cr.Errors.HasErrors)
+            {
+                var message = new StringBuilder();
+                message.Append("Error compiling generated files.\n");
+                foreach(var error in cr.Errors)
+                    message.AppendFormat("  {0}\n",error.ToString());
+                throw new Exception(message.ToString());
+            }
+        }
+    }
+}
