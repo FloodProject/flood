6870e5aced24713f834cb2182ac87cc65f3b100b
marcos henrich <marcoshenrich@hotmail.com>
2013-10-08 18:26:02 +0100

    Runtime now only creates and runs Application.

--- src/Runtime/Runtime.cs
+++ src/Runtime/Runtime.cs
@@ -1,25 +1,13 @@
 
-using System.Threading;
-using Flood.Network;
-using Flood.Packages;
-using Flood.Remoting;
-
 namespace Flood.Editor
 {
     public class Runtime
     {
-        public static PackageManager packageManager;
-
         public static void Main(string[] args)
         {
-            var messageProcessor = new MessageProcessor(new SessionRemotingPeer(null), new PackageContextLoader());
+            var app = new Application();
 
-            packageManager = new PackageManager("Packages", messageProcessor);
-            while (true)
-            {
-                packageManager.UpdatePackages();
-                Thread.Sleep(1);
-            }
+            app.Run();
         }
     }
 }
