c83204b96039adc37de242db106f22a488d3bf35
marcos henrich <marcoshenrich@hotmail.com>
2013-10-11 14:13:03 +0100

    Added IModule implementation to TextAddin.

---
+++ src/Editor/Addins/TextAddin/Module.cs
@@ -0,0 +1,27 @@
+﻿
+using Editor.Services;
+using Flood.Modules;
+using TextAddin.Controls;
+using TextAddin.Document;
+
+namespace TextAddin
+{
+    class Module : IModule
+    {
+        public void OnLoad(Flood.Remoting.ServiceManager serviceManager)
+        {
+            var paneManager = serviceManager.GetGlobalService<IPaneManager>();
+
+            var textDocument = new TextDocument();
+            var textView = new TextView(null, textDocument);
+
+            var pane = new Pane() {Title = "CodeEdit", control = textView};
+            paneManager.AddPane(pane);
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
