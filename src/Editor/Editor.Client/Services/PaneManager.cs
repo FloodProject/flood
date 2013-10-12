a8c4c9dfb8f8c1469a4f028c81407f9a05320104
marcos henrich <marcoshenrich@hotmail.com>
2013-10-12 01:10:18 +0100

    PaneManager now keeps track of the focused pane groups.

--- src/Editor/Editor.Client/Services/PaneManager.cs
+++ src/Editor/Editor.Client/Services/PaneManager.cs
@@ -1,5 +1,8 @@
 ﻿
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Flood.Editor.Client.GUI;
+using Flood.GUI;
 using Flood.Remoting.Metadata;
 using Flood.GUI.Controls;
 
@@ -25,9 +28,42 @@ namespace Editor.Services
     // Implementation of global service IPaneManager
     public class PaneManager : IPaneManager
     {
+        private Container container;
+        private LinkedList<PaneGroup> FocusedGroup;
+
+        public PaneManager(Canvas canvas)
+        {
+            container = new Container(canvas);
+            container.Dock = Pos.Fill;
+
+            FocusedGroup = new LinkedList<PaneGroup>();
+        }
+
+        public PaneGroup Focus
+        {
+            get { return FocusedGroup.First.Value; }
+            set
+            {
+                FocusedGroup.Remove(value);
+                FocusedGroup.AddFirst(value);
+            }
+        }
+
+        public void RemovePane(PaneGroup paneGroup)
+        {
+            FocusedGroup.Remove(paneGroup);
+        }
+
         public async Task AddPane(Pane pane)
         {
-            
+            if (FocusedGroup.Count == 0)
+            {
+                var paneGroup = new PaneGroup(container, this);
+                container.InsertPanel(paneGroup);
+                Focus = paneGroup;
+            }
+
+            Focus.AddPane(pane);
         }
     }
 }
