5addaeba2de35d63ffd29a889dcb7169b7951c65
triton <ripzonetriton@gmail.com>
2014-01-21 20:56:52 +0000

    Ignore the MathClamp function template for binding purposes.

--- inc/Core/Math/Helpers.h
+++ inc/Core/Math/Helpers.h
@@ -30,7 +30,8 @@ API_CORE float InterpolateCosine(float a, float b, float t);
 API_CORE float InterpolateCubic(float a, float b, float t);
 
 // Clamps a variable to the given range.
-template< typename T > bool MathClamp( T& var, T min = 0, T max = 1 )
+template< typename T > FLD_IGNORE
+bool MathClamp( T& var, T min = 0, T max = 1 )
 {
 	// Equivalent to: max(min, min(var, max))
 	if( var >= max )
