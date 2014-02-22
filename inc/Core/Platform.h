7420fc08f6d7739bc37794d085620d1d728eed13
triton <ripzonetriton@gmail.com>
2014-02-22 18:29:16 +0000

    Added two new macros supported by CppSharp.

--- inc/Core/Platform.h
+++ inc/Core/Platform.h
@@ -216,6 +216,8 @@ typedef uint32 uint;
 #define FLD_OUT
 #define FLD_IN_OUT
 #define FLD_IGNORE
+#define FLD_IGNORE_GEN
+#define FLD_IGNORE_FILE
 #define FLD_FLAGS
 #define FLD_HASHCODE
 #define FLD_EQUALS
